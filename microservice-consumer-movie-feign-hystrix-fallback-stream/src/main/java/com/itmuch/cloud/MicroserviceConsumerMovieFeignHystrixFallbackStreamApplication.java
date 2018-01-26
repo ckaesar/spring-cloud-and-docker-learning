6ec6525942e91b7f342f635983265afa874f9d89
chengkai <chengkai@glorystone.net>
2018-01-26 19:24:42 +0800

    add hystrix and turbine

---
+++ microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/itmuch/cloud/MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication.java
@@ -0,0 +1,20 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+public class MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConsumerMovieFeignHystrixFallbackStreamApplication.class, args);
+	}
+}
